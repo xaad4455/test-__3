f332f5f49d157bf6f59fdc5853762286f5a1a5f6
mahdi alin <mahadaalin@gmail.com>
2025-09-14 04:48:31 -0700

    main commit 1
    Initial commit of Cards class project
    Setup basic project structure and directories
    Add JFrame initialization and basic window settings
    Setup JPanel and CardLayout for card switching

---
+++ untitled/src/Cards.java
@@ -0,0 +1,12 @@
+public class Cards import javax.swing.*;
+        import java.awt.*;
+public class Cards {
+
+    JFrame frame;
+    JPanel cardPanel;
+    CardLayout cardLayout;
+
+    public Cards() {
+
+        
+}
